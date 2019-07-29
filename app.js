4ea5242756510a2b030d07f3fc0b625cd2f0aa5b
Lettisu <Susanna.Lettieri@verizon.com>
2019-07-29 10:37:56 -0400

    removed History

--- app.js
+++ app.js
@@ -1,60 +1,50 @@
-const express = require("express");
-//const router = express.Router();
-const data = require("./data/data.json");
-const app = express();
-const projects = data.projects
+  //begin to build portfolio
+  const express = require("express");
+  const data = require("./data/data.json");
+  const app = express();
+  const projects = data.projects
 
-//view engine setup
-app.set('view engine', 'pug');
+  //view engine setup
+  app.set('view engine', 'pug');
 
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use('/static', express.static('public'));
+  //static files
+  app.use('/static', express.static('public'));
 
+  app.get('/', (req, res, ) => {
+      const data = {
+          projects
+      };
+      res.render('index', data);
+  });
 
-//app.use(mainRouter);
-//app.use('./routes/project');
+  app.get('/about', (req, res, next) => {
+      res.render('about');
+  });
 
+  app.get('/project/:id', (req, res, next) => {
+      const {
+          id
+      } = req.params;
+      if (id && id < projects.length) {
+          res.render("project", projects[id]);
+      } else {
+          err(next);
+      }
 
+  });
+  app.use((req, res, next) => {
+      const err = new Error('Page not found');
+      err.status = 404;
+      next(err);
+  });
+  app.use((req, res, next) => {
+      console.log(err.message);
+      res.status(err.status);
+      res.render('error', {
+          error: err
+      })
+  })
 
-//local mods
-// const mainRouter = require('./routes');
-// const pjsRoute = require('./routes/project');
-
-//static files
-app.use('/static', express.static('public'));
-// app.use(mainRouter);
-// app.use('/project', pjsRoute);
-
-
-//const projects = projects
-app.get('/', (req, res, ) => {
-    const data = {
-        projects
-    };
-    res.render('index', data);
-});
-
-app.get('/about', (req, res, next) => {
-    res.render('about');
-});
-
-
-// middleware error
-// app.use((req, res, next) => {
-//     const err = new Error("Page not found");
-//     err.status = 404;
-//     next(err);
-// });
-
-// app.use((err, req, res, next) => {
-//     console.log(err.message);
-
-//     res.status(err.status);
-//     res.render('error', {
-//         error: err
-//     });
-// });
-
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
-});
+  app.listen(3000, () => {
+      console.log('The application is running on localhost:3000!')
+  });
